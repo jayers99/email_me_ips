403edada54343eca6cb622d00dc7e613ffd082b2
John Ayers <jayers99@gmail.com>
2024-08-16 13:21:41 -0700

    chore: Add get_emls function to retrieve email addresses

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python3
+
+
+def get_emls():
+	emls = ["email1@example.com", "email2@example.com"]
+	return emls
+
+
 def main():
 	print("Extracting IP addresses from emails...")
+
 
 if __name__ == "__main__":
 	main()
